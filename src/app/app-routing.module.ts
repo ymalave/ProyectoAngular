76610b275fd87cd1d6b2f541d5c2ace60993d122
ymalave~ <ymalave@seguroscaroni.com>
2022-12-19 14:05:04 -0400

    ys

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { ReadEmployeesComponent } from './components/read-employees/read-employees.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'add-employee'},
+  {path: 'add-employee', component: AddEmployeeComponent},
+  {path:'read-employees', component: ReadEmployeesComponent},
+  {path: 'update-employee', component: UpdateEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
